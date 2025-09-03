f56a889fd5ff401895ed463ce98a674431fdc1b0
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-09-03 11:08:01 -0600

    Add files via upload

---
+++ ProjectHorizon/includes/layouts/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+    <div class="header-left">
+        <div class="header-item">
+            <div class="header-button">
+                <span class="material-symbols-rounded">menu</span>
+            </div>
+        </div>
+    </div>
+    <div class="header-right">
+        <div class="header-item">
+            <div class="header-button">
+                <span class="material-symbols-rounded">settings</span>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ ProjectHorizon/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <div class="main-content">
+            <div class="general-content">
+                <div class="general-content-top">
+                    <?php include 'includes/layouts/header.php'; ?>
+                </div>
+                <div class="general-content-bottom"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
