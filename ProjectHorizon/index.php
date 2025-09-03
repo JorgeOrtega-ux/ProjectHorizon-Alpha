a01693dc1aa73b53ec15d100c080147d65666105
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-09-03 11:22:57 -0600

    Add files via upload

--- ProjectHorizon/index.php
+++ ProjectHorizon/index.php
@@ -16,7 +16,36 @@
                 <div class="general-content-top">
                     <?php include 'includes/layouts/header.php'; ?>
                 </div>
-                <div class="general-content-bottom"></div>
+                <div class="general-content-bottom">
+                    <div class="module-content module-surface">
+                        <div class="menu-content">
+                            <div class="menu-list">
+                                <div class="menu-link active" data-action="toggleSectionHome">
+                                    <div class="menu-link-icon">
+                                        <span class="material-symbols-rounded">home</span>
+                                    </div>
+                                    <div class="menu-link-text">
+                                        <span>Pagina principal</span>
+                                    </div>
+                                </div>
+                                <div class="menu-link" data-action="toggleSectionExplore">
+                                    <div class="menu-link-icon">
+                                        <span class="material-symbols-rounded">action_key</span>
+                                    </div>
+                                    <div class="menu-link-text">
+                                        <span>Explorar categorias</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="general-content-scrolleable">
+                        <div class="section-container">
+                            <div class="section-content active" data-section="sectionHome">1</div>
+                            <div class="section-content disabled" data-section="sectionExplore">2</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
