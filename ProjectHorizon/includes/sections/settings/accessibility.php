cbb29eb855972c3da774df5e9c805081f1901fb5
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-09-05 10:33:51 -0600

    Add files via upload

---
+++ ProjectHorizon/includes/sections/settings/accessibility.php
@@ -0,0 +1,83 @@
+<div class="section-content <?php echo ($CURRENT_SECTION === 'accessibility') ? 'active' : 'disabled'; ?>" data-section="accessibility">
+    <div class="settings-page-container">
+        <div class="content-section header-section">
+            <div class="item-details">
+                <h2>Accesibilidad</h2>
+                <p>Configura las opciones de accesibilidad para adaptar la interfaz a tus necesidades.</p>
+            </div>
+        </div>
+        <div class="content-section select-section">
+            <div class="item-details">
+                <h4>Idioma</h4>
+                <p>Elige tu idioma de preferencia para la interfaz.</p>
+            </div>
+            <div class="form-controls">
+                <div class="select-wrapper body-title">
+                    <div class="custom-select-trigger" data-module="moduleLanguage">
+                        <span class="select-trigger-text">Español (Latinoamérica)</span>
+                        <div class="select-trigger-icon">
+                            <span class="material-symbols-rounded">expand_more</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-section select-section">
+            <div class="item-details">
+                <h4>Tema</h4>
+                <p>Personaliza la apariencia de tu cuenta. Selecciona un tema o sincroniza temas con las preferencias de tu sistema.</p>
+            </div>
+            <div class="form-controls">
+                <div class="select-wrapper body-title">
+                    <div class="custom-select-trigger" data-module="moduleLanguage">
+                        <span class="select-trigger-text">Sincronizar con el sistema</span>
+                        <div class="select-trigger-icon">
+                            <span class="material-symbols-rounded">expand_more</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-section data-group-section">
+            <div class="data-item">
+                <div class="view-container active">
+                    <div class="item-details">
+                        <h4>Los atajos necesitan un modificador</h4>
+                        <p>Para crear atajos, es necesario usar la tecla modificadora Alt.</p>
+                    </div>
+                    <div class="item-actions">
+                        <div class="toggle-switch">
+                            <div class="toggle-handle"><span class="material-symbols-rounded">check</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="data-item">
+                <div class="view-container active">
+                    <div class="item-details">
+                        <h4>Contraste alto de colores</h4>
+                        <p>Se mantiene un mayor contraste entre el texto y el fondo, incluidos los fondos con degradados.</p>
+                    </div>
+                    <div class="item-actions">
+                        <div class="toggle-switch">
+                            <div class="toggle-handle"><span class="material-symbols-rounded">check</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="data-item">
+                <div class="view-container active">
+                    <div class="item-details">
+                        <h4>Subtítulos</h4>
+                        <p>Los subtítulos se generarán y se mostrarán en todo el contenido que tenga audios o videos con diálogo.</p>
+                    </div>
+                    <div class="item-actions">
+                        <div class="toggle-switch">
+                            <div class="toggle-handle"><span class="material-symbols-rounded">check</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- ProjectHorizon/index.php
+++ ProjectHorizon/index.php
@@ -1,6 +1,7 @@
 <?php require_once 'config/router.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,12 +12,13 @@
     <link rel="stylesheet" type="text/css" href="/ProjectHorizon/assets/css/styles.css">
     <title>My Dynamic Website</title>
 </head>
+
 <body>
     <div class="page-wrapper">
         <div class="main-content">
             <div class="general-content">
                 <div class="general-content-top">
-             <?php include 'includes/layouts/header.php'; ?>
+                    <?php include 'includes/layouts/header.php'; ?>
                 </div>
                 <div class="general-content-bottom">
                     <div class="module-content module-surface disabled" data-module="moduleSurface">
@@ -47,7 +49,7 @@
                                     <div class="menu-link-icon"><span class="material-symbols-rounded">history</span></div>
                                     <div class="menu-link-text"><span>Historial y Privacidad</span></div>
                                 </div>
-                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -55,11 +57,14 @@
                         <div class="section-container <?php echo ($CURRENT_VIEW === 'main') ? 'active' : 'disabled'; ?>" data-view="main">
                             <div class="section-content <?php echo ($CURRENT_SECTION === 'home') ? 'active' : 'disabled'; ?>" data-section="home">Contenido de Home</div>
                             <div class="section-content <?php echo ($CURRENT_SECTION === 'explore') ? 'active' : 'disabled'; ?>" data-section="explore">Contenido de Explorar</div>
-                            <div class="section-content <?php echo ($CURRENT_SECTION === '404') ? 'active' : 'disabled'; ?>" data-section="404"><h1>404 - Not Found</h1><p>The page you are looking for does not exist.</p></div>
+                            <div class="section-content <?php echo ($CURRENT_SECTION === '404') ? 'active' : 'disabled'; ?>" data-section="404">
+                                <h1>404 - Not Found</h1>
+                                <p>The page you are looking for does not exist.</p>
+                            </div>
                         </div>
 
                         <div class="section-container <?php echo ($CURRENT_VIEW === 'settings') ? 'active' : 'disabled'; ?>" data-view="settings">
-                            <div class="section-content <?php echo ($CURRENT_SECTION === 'accessibility') ? 'active' : 'disabled'; ?>" data-section="accessibility">Contenido de Accesibilidad</div>
+                            <?php include 'includes/sections/settings/accessibility.php'; ?>
                             <div class="section-content <?php echo ($CURRENT_SECTION === 'historyPrivacy') ? 'active' : 'disabled'; ?>" data-section="historyPrivacy">Contenido de Historial y Privacidad</div>
                         </div>
                     </div>
@@ -67,7 +72,8 @@
             </div>
         </div>
     </div>
-    
+
     <script type="module" src="/ProjectHorizon/assets/js/app-init.js"></script>
 </body>
+
 </html>
