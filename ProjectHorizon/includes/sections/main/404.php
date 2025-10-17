c3c1ff6e8a025735a3121715919d41f92ad61875
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-10-17 13:47:34 -0500

    Delete ProjectHorizon directory

--- ProjectHorizon/includes/sections/main/404.php
+++
@@ -1,7 +0,0 @@
-<div class="section-content <?php echo ($CURRENT_SECTION === '404') ? 'active' : 'disabled'; ?>" data-section="404">
-    <div class="not-found-container">
-        <span class="not-found-badge" data-i18n="404.badge"></span>
-        <h1 data-i18n="404.title"></h1>
-        <p data-i18n="404.message"></p>
-    </div>
-</div>
--- ProjectHorizon/includes/sections/main/access-code-prompt.php
+++
@@ -1,9 +0,0 @@
-<div class="section-content <?php echo ($CURRENT_SECTION === 'accessCodePrompt') ? 'active' : 'disabled'; ?>" data-section="accessCodePrompt">
-    <div class="access-code-container">
-        <h2 id="access-code-title" data-i18n="accessCodePrompt.privateGallery"></h2>
-        <p data-i18n="accessCodePrompt.unlockMessage"></p>
-        <div class="access-code-form">
-            <button id="watch-ad-button" class="load-more-btn" data-action="watch-ad-to-unlock" data-i18n="accessCodePrompt.unlockButton"></button>
-        </div>
-    </div>
-</div>
